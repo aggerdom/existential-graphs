d4be9498bf74c6dca37349100e48ded5f2877cb7
Alex Gerdom <alexgerdom@gmail.com>
2020-08-30 21:23:39 -0400

    Initial Prototype
    - Move tests into correct place
    - Get all rules defined

--- src/existential_graphs/core.clj
+++ src/existential_graphs/core.clj
@@ -1,64 +1,112 @@
 (ns existential-graphs.core
-  (:require [existential-graphs.tree :as T]
-   [dorothy.core :as dot]
-            [dorothy.jvm :refer (render save! show!)]))
+  (:require [spectacles.lenses :as lens]))
 
-;; Define data structure
-;; ENTITIES
-;; :SA  := Sheet of assertion
-;; :cut := logical negation, increases depth by one for it's children
-;; :prop := proposition
+;; Helpers
+(defn path-depth [path] (+ 2 (count (take-while #(not (nil? %)) path))))
+(defn is-double-cut? [G path]
+  (let [node (get-in G path)]
+    (and (< 0 (count node))
+         (= (first node) :cut) ;; Node value is :cut
+         (= (count node) 2) ;; No other children but second cut
+         (= (first (second node)) :cut))))
 
-(def example-tree
-  [:SA
-   [:A]
-   [:cut
-    [:B]
-    [:cut
-     [:C]]]])
+(defn exists-ancestral-copy? [G path] true) ;; TODO: Implement
+(defn have-common-parent? [G src-path dest-path] true) ;; TODO: Implement
 
-;; Common Definitions
-;; Adjusted Definitions
+(defn insert [G subG path]
+  "Insert node as a child of a specific path into graph.
+   Empty path places node as child of the root."
+  (cond (empty? path) (conj G subG)
+        (nil? (first path)) (conj G subG)
+        :else (update G (first path)
+                      #(insert % subG (rest path)))))
 
-(do "helper-definitions"
-    (defn leaf? [node] (= (count node) 1))
-    (defn children? [node] (> (count node) 1))
-    (defn node-value [node] (first node))
-    (defn node-children [node] (rest node))
-    (defn add-child-l [p c] (into [(first p) c] (rest p)))
-    (defn add-child-r [p c] (conj p c))
-    (defn node-count [T]
-      (loop [count 0, stack [T]]
-        (if (empty? stack) count
-            (recur (inc count)
-                   (concat (rest (first stack))
-                           (rest stack))))))
-    (defn edges [T]
-      (if (leaf? T) []
-          (concat
-           ()
-      )
-    ))
+(defn replace-node [G f path]
+  "Replace node at a specific position with a new node obtained by
+   calling f on the existing val"
+  (if (= (count path) 1) (update G (first path) #(f %))
+      (update (first path) #(replace-node % f (rest path)))))
 
-(defn visualize-tree [T]
-  (let [edges]))
+(defn drop-vec-position [v ix]
+  (into (subvec v 0 ix)
+        (subvec v (inc ix))))
 
 
-;; RULES
-(let [g (dot/graph [
-                    [:22 :2]
-                    [:8 :2]
-                    [:10 :5 :1]
-                    [:10 :2 :1]])]
-  (-> g dot/dot show!))
+(defn erase [G path]
+  {:pre [(< 0 (count path))]}
+  (if (= (count path) 1) (drop-vec-position G (first path))
+      (update G (first path) #(erase G (rest path)))))
 
-(comment "R.I.I. Any subgraph may be inserted at an odd numbered level.
-         R.I.E. Any subgraph at an even numbered level may be erased.
-         R.E.D.I. A pair of cuts with nothing between them may be drawn
-         around any subgraph.
-         R.E.D.E. A pair of cuts with nothing between them may be erased.
-         Iteration: Any subgraph P depending on node n
-                    may be copied into any node depending on n
-         Deiteration: Any subgraph P in node n may be erased if there is
-                    a copy of it in a node ancestral to n")
+
+;; Rules ==================================================================
+;;      SA     -- 2
+;;     cut     -- 3
+;; A B cut     -- 4 (Insertion "in odd"=add B as child of odd number of cuts) [1 nil]
+;;      C      -- 5 (Erasure "in even" = we can erase C) [1 1 1] (<- path is odd len)
+;; Specify path to place child (path points to parent)
+(defn insertion [G subG path]
+  {:pre [(odd? (path-depth path))]
+   :post []
+   :docstring "Any subgraph may be inserted at an odd numbered depth"}
+  (insert G subG path))
+
+
+;; Specify path of child to erase (path points to node to erase)
+(defn erasure [G path]
+  {:pre [(even? (dec (path-depth path)))] ;; We can erase at any even depth (root is 2)
+   :post []
+   :docstring "Any subgraph in an even depth may be erased"}
+  (erase G path))
+
+
+(defn double-cut [G path]
+  ;; If path points to a node, then:
+  ;;  - double cut will be placed around that node
+  ;;  - That node will be placed as a child under two cuts in its present pos
+  ;; If path ends in nil, then:
+  ;;  - two cuts will be placed as a child of (butlast node)
+  ;;  - A double cut will be created without any children
+  {:pre []
+   :post []
+   :docstring (str "A pair of cuts w nothing between them may be drawn"
+                   "around any subgraph (including empty graph)")}
+  (cond (= path []) (throw "Not understood")
+        ;; CASE: Empty double-cut
+        (and (= 1 (count path)) (nil? (first path)))
+        (conj G [:cut [:cut]])
+        ;; CASE: Cut around node
+        (= 1 (count path))
+        (update G (first path) (fn [cur-node] [:cut [:cut cur-node]]))
+        ;; CASE: We're not drilled down yet
+        :else (update G (first path) #(double-cut % (rest path)))))
+
+(defn double-cut-erasure [G path]
+  {:pre [(is-double-cut? G path)]
+   :post []
+   :docstring "A pair of cuts w nothing between them may be erased"}
+  ;; Path points to first cut
+  (let [G1 G
+        [_ [_ & children]] (get-in G path)
+        ;; Append all children of second cut to the parent
+        G2 (into (get-in G1 (butlast path)) children)
+        ;; Remove the second cut from the altered parent
+        G3 (erase G2 path)]
+    G3))
+
+(defn iteration [G src-path dest-path]
+  {:pre [(have-common-parent? G src-path dest-path)]
+   :post []
+   :docstring (str "Any subgraph P depending on node n may be copied"
+                   "into any node depending on n.")}
+  (insert G (get-in G src-path) dest-path))
+
+(defn deiteration [G src-path]
+  {:pre [(exists-ancestral-copy? src-path)]
+   :post []
+   :docstring (str "Any subgraph P in node n may be erased if there is"
+                   "a copy of it in a node ancestral to n."
+                   "(A node on which n depends which n depends)")}
+  (erase G src-path))
+
+
 
--- test/existential_graphs/core_test.clj
+++ test/existential_graphs/core_test.clj
@@ -1,20 +1,86 @@
 (ns existential-graphs.core-test
-  (:require [clojure.test :refer :all]
-            [existential-graphs.core :refer :all]))
+  (:require [existential-graphs.core :as sut]
+            [clojure.test :refer :all]))
 
-;; (deftest a-test
-  ;; (testing "FIXME, I fail."
-    ;; (is (= 0 1))))
+(count (take-while #(not (nil? %)) [nil]))
 
-(deftest add-children-test
-  (is (= (add-child-l [:A [:B]] [:C]) [:A [:C] [:B]]))
-  (is (= (add-child-r [:A [:B]] [:C]) [:A [:B] [:C]])))
+;; Depth starts at 2
+;; Each cut it passes through adds one
+(deftest path-depth-test
+  (is (= (sut/path-depth [nil]) 2))
+  (is (= (sut/path-depth [1 nil]) 3)))
 
-(deftest children?-test
-  (is (= (children? []) false))
-  (is (= (children? [:A [:B]]) true))
-  (is (= (children? [:A]) false)))
+(deftest is-double-cut?-test
+  (is (= (sut/is-double-cut? [:SA [:cut [:cut]]] []) false))
+  (is (= (sut/is-double-cut? [:SA [:cut [:cut]]] []) false))
+  (is (= (sut/is-double-cut? [:SA [:cut [:cut]]] [1]) true))
+  (is (= (sut/is-double-cut? [:SA [:cut [:A [:cut]]]] [1]) false))
+  (is (= (sut/is-double-cut? [:SA [:A] [:cut [:cut]]] [1]) false))
+  (is (= (sut/is-double-cut? [:SA [:A] [:cut [:cut]]] [2]) true))
+  (is (= (sut/is-double-cut? [:SA [:cut [:cut [:A]]]] [1]) true))
+  (is (= (sut/is-double-cut? [:SA [:cut [:cut [:A]]]] [1 2 3]) false))) ;; nonexistent path
 
+(deftest exists-ancestral-copy?-test
+  ;; (is false "WRITE TEST")
+  )
+(deftest have-common-parent?-test
+  ;; (is false "WRITE TEST")
+  )
 
+(deftest insert-test
+  (is (= (sut/insert [:SA] [:cut [:cut]] []) [:SA [:cut [:cut]]]))
+  (is (= (sut/insert [:SA [:A]] [:B] [nil]) [:SA [:A] [:B]]))
+  (is (= (sut/insert [:SA [:A]] [:B] [1 nil]) [:SA [:A [:B]]])))
 
+(deftest replace-node-test
+  (is (= (sut/replace-node [:A [:B]]  (fn [x] (vector :C x))   [1])
+         [:A [:C [:B]]]))
+  (is (= (sut/replace-node [:SA [:A]] (fn [x] [:cut [:cut x]]) [1])
+       [:SA [:cut [:cut [:A]]]]))
+  )
 
+(deftest drop-vec-position-test
+  (is (= (sut/drop-vec-position [1 2 3] 0) [2 3]))
+  (is (= (sut/drop-vec-position [1 2 3] 1) [1 3])))
+
+(deftest erase-test
+  (is (= (sut/erase [:SA [:A]] [1]) [:SA])))
+
+;; http://users.clas.ufl.edu/jzeman/graphicallogic/introduction.htm
+(deftest insertion-test
+  ;; Insertion "in odd"
+  ;; Should work
+  (is (= (sut/insertion [:SA [:cut]]
+                        [:A]
+                        [1 nil])
+         [:SA [:cut [:A]]]) "Can Insert on even (Under an odd node)")
+  ;; Should error
+  (is (thrown? AssertionError (sut/insertion [:SA] [:A] [nil]))))
+
+(deftest erasure-test
+  (testing "Erasure At Even Depth"
+    (is (= (sut/erasure [:SA [:A] [:cut [:B]]] [1]) [:SA [:cut [:B]]]))
+    (is (= (sut/erasure [:SA [:A] [:cut [:B]]] [2]) [:SA [:A]])))
+  (testing "Erasure At Odd Depth"
+    (is (thrown? AssertionError
+                 (sut/erasure [:SA [:A] [:cut [:B]]] [2 1])))))
+
+(deftest double-cut-test
+  ;; Examples
+  (is (= (sut/double-cut [:SA] [nil]) [:SA [:cut [:cut]]]))
+  (is (= (sut/double-cut [:SA [:A]] [nil])  [:SA [:A] [:cut [:cut]]]))
+  (is (= (sut/double-cut [:SA [:A]] [1])  [:SA [:cut [:cut [:A]]]]))
+  (is (= (sut/double-cut [:SA [:A]] [1 nil])  [:SA [:A [:cut [:cut]]]]))
+  (is (= (sut/double-cut [:SA [:A [:B [:C]]]] [1 1])  [:SA [:A [:cut [:cut [:B [:C]]]]]])))
+
+(deftest double-cut-erasure-test
+  (is (= (sut/double-cut-erasure [:SA [:cut [:cut [:A] [:B]]]] [1])
+         [:SA [:A] [:B]])))
+(deftest iteration-test
+  (is (= (sut/iteration [:SA [:A]] [1] [1 nil])
+         [:SA [:A [:A]]])))
+
+(deftest deiteration-test
+  (is
+   (= (sut/double-cut-erasure [:SA [:cut [:cut [:A] [:B]]]] [1])
+      [:SA [:A] [:B]])))
